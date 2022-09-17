aafeba45e13d7244cfa0b7f3739a7cb4148f47db
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-17 19:52:20 +0300

    Add files via upload

---
+++ Ages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1._Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
