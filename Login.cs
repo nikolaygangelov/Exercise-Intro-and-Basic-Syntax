458b6ac23ffd846bbe270a9ad2bdc9a84c37e4e1
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-19 00:33:55 +0300

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            
+
+            string password = string.Empty;
+            int counter = 0;
+
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                char letter = username[i];
+                password += letter;    
+            }
+            string enteredPassword;
+            while ((enteredPassword = Console.ReadLine()) != password)
+            {
+                if (enteredPassword != password)
+                {
+                    counter++;
+                    if (counter >= 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break;
+                    }
+                    Console.WriteLine($"Incorrect password. Try again.");
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                }
+            }
+            if (counter < 4)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+        }
+    }
+}
