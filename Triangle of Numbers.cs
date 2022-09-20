d12628160767c00c12bdf9389b5cc3a87f3dd800
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-20 22:38:09 +0300

    Add files via upload

---
+++ Triangle of Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08._Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int col = 1; col <= rows; col++)
+                {
+                    Console.Write($"{rows} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
